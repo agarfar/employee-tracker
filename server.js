39b3a5f70befd1c74c7f38960d036bc5a0e1b563
Soprano <antfar67@gmail.com>
2022-12-20 23:06:56 -0800

    Substitued async await for .then statements, added getDepartments, getRoles, and getEmployees to various functions

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const inquirer = require("inquirer");
 // const mysql = require('mysql2/promise');
 require('dotenv').config();
 // const cTable = require('console.table');
-const { departmentList, departmentQuestions, viewDepartments, addDepartment } = require('./lib/department');
-const { roleList, roleQuestions, viewRoles, addRole } = require('./lib/role');
-const { employeeQuestions, updateQuestions, viewEmployees, addEmployee, updateRole } = require('./lib/employee');
+const { viewDepartments, addDepartment, getDepartments } = require('./lib/department');
+const { viewRoles, addRole } = require('./lib/role');
+const { viewEmployees, addEmployee, updateRole } = require('./lib/employee');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,65 +25,231 @@ const nextActionQuestion = [
     },
 ]
 
+// let departmentList;
+// let employeeList;
+// let roleList;
+
+// const roleQuestions = [
+//     {
+//         type: "input",
+//         message: "Enter role name:",
+//         name: "name",
+//     },
+
+//     {
+//         type: "number",
+//         message: "Enter salary",
+//         name: "salary",
+//     },
+
+//     {
+//         type: "list",
+//         message: "Choose department:",
+//         name: "department",
+//         choices: departmentList,
+//     },
+// ];
+
+// const updateQuestions = [
+//     {
+//         type: "list",
+//         message: "Choose employee whose role you need to update:",
+//         name: "employee",
+//         choices: employeeList,
+//     },
+
+//     {
+//         type: "list",
+//         message: "Choose employee's new role",
+//         name: "role",
+//         choices: roleList,
+//     },
+
+// ];
+
+// // const employeeQuestions = [
+//     {
+//         type: "input",
+//         message: "Enter employee's first name:",
+//         name: "firstName",
+//     },
+
+//     {
+//         type: "input",
+//         message: "Enter employee's last name:",
+//         name: "lastName",
+//     },
+
+//     {
+//         type: "list",
+//         message: "Enter employee's role",
+//         name: "role",
+//         choices: roleList,
+//     },
+
+//     {
+//         type: "list",
+//         message: "Enter employee's manager:",
+//         name: "manager",
+//         choices: employeeList,
+//     },
+// ];
+
 // Prompt to perform another action
-const nextActionPrompt = () => {
-    return inquirer
-        .prompt(nextActionQuestion)
-        .then((answer) => {
-            switch (answer.nextAction) {
-                case 'View all departments':
-                    // code block
-                    viewDepartments().then(() => nextActionPrompt());
-                    break;
-                case 'View all employees':
-                    // code block
-                    viewEmployees().then(() => nextActionPrompt());
-                    break;
-                case 'View all roles':
-                    // code block
-                    viewRoles().then(() => nextActionPrompt());
-                    break;
-                case 'Add a department':
-                    // code block
-                    inquirer
-                        .prompt(departmentQuestions)
-                        .then((answer) => {
-                            return addDepartment(answer.name);
-                        })
-                        .then(() => nextActionPrompt());
-                    break;
-                case 'Add a role':
-                    // code block
-                    inquirer
-                        .prompt(roleQuestions)
-                        .then((answer) => {
-                            return addRole(answer.name, answer.salary, answer.department);
-                        })
-                        .then(() => nextActionPrompt());
-                    break;
-                case 'Add an employee':
-                    // code block
-                    inquirer
-                        .prompt(employeeQuestions)
-                        .then((answer) => {
-                            return addEmployee(answer.firstName, answer.lastName, answer.role, answer.manager);
-                        })
-                        .then(() => nextActionPrompt());
-                    break;
-                case 'Update an employee role':
-                    // code block
-                    inquirer
-                        .prompt(updateQuestions)
-                        .then((answer) => {
-                            return updateRole(answer.employee, answer.role);
-                        })
-                        .then(() => nextActionPrompt());
-                    break;
-                case 'Exit the application':
-                    // code block
-                    process.exit(0);
-            }
-        })
+// const nextActionPrompt = () => {
+//     return inquirer
+//         .prompt(nextActionQuestion)
+//         .then((answer) => {
+//             switch (answer.nextAction) {
+//                 case 'View all departments':
+//                     // code block
+//                     viewDepartments().then(() => nextActionPrompt());
+//                     break;
+//                 case 'View all employees':
+//                     // code block
+//                     viewEmployees().then(() => nextActionPrompt());
+//                     break;
+//                 case 'View all roles':
+//                     // code block
+//                     viewRoles().then(() => nextActionPrompt());
+//                     break;
+//                 case 'Add a department':
+//                     // code block
+//                     inquirer
+//                         .prompt(departmentQuestions)
+//                         .then((answer) => {
+//                             return addDepartment(answer.name);
+//                         })
+//                         .then(() => nextActionPrompt());
+//                     break;
+//                 case 'Add a role':
+//                     // code block
+//                     inquirer
+//                         .prompt(roleQuestions)
+//                         .then((answer) => {
+//                             return addRole(answer.name, answer.salary, answer.department);
+//                         })
+//                         .then(() => nextActionPrompt());
+//                     break;
+//                 case 'Add an employee':
+//                     // code block
+//                     inquirer
+//                         .prompt(employeeQuestions)
+//                         .then((answer) => {
+//                             return addEmployee(answer.firstName, answer.lastName, answer.role, answer.manager);
+//                         })
+//                         .then(() => nextActionPrompt());
+//                     break;
+//                 case 'Update an employee role':
+//                     // code block
+//                     inquirer
+//                         .prompt(updateQuestions)
+//                         .then((answer) => {
+//                             return updateRole(answer.employee, answer.role);
+//                         })
+//                         .then(() => nextActionPrompt());
+//                     break;
+//                 case 'Exit the application':
+//                     // code block
+//                     process.exit(0);
+//             }
+//         })
+// };
+
+// const nextActionPrompt = async () => {
+//     let answer = await inquirer.prompt(nextActionQuestion);
+//     switch (answer.nextAction) {
+//         case 'View all departments':
+//             // code block
+//             await viewDepartments()
+//             nextActionPrompt();
+//             break;
+//         case 'View all employees':
+//             // code block
+//             await viewEmployees()
+//             nextActionPrompt();
+//             break;
+//         case 'View all roles':
+//             // code block
+//             await viewRoles()
+//             nextActionPrompt();
+//             break;
+//         case 'Add a department':
+//             // code block
+//             let departmentAnswer = await inquirer.prompt(departmentQuestions);
+//             await addDepartment(departmentAnswer.name);
+//             nextActionPrompt();
+//             break;
+//         case 'Add a role':
+//             // code block
+//             let roleAnswer = await inquirer.prompt(roleQuestions);
+//             addRole(roleAnswer.name, roleAnswer.salary, roleAnswer.department);
+//             nextActionPrompt();
+//             break;
+//         case 'Add an employee':
+//             // code block
+//             let employeeAnswer = inquirer.prompt(employeeQuestions);
+//             await addEmployee(employeeAnswer.firstName, employeeAnswer.lastName, employeeAnswer.role, employeeAnswer.manager);
+//             nextActionPrompt();
+//             break;
+//         case 'Update an employee role':
+//             // code block
+//             let updateAnswer = await inquirer.prompt(updateQuestions)
+//             await updateRole(updateAnswer.employee, updateAnswer.role);
+//             nextActionPrompt();
+//             break;
+//         case 'Exit the application':
+//             // code block
+//             process.exit(0);
+//     }
+// };
+
+const nextActionPrompt = async () => {
+    let answer = await inquirer.prompt(nextActionQuestion);
+    switch (answer.nextAction) {
+        case 'View all departments':
+            // code block
+            await viewDepartments()
+            nextActionPrompt();
+            break;
+        case 'View all employees':
+            // code block
+            await viewEmployees()
+            nextActionPrompt();
+            break;
+        case 'View all roles':
+            // code block
+            await viewRoles()
+            nextActionPrompt();
+            break;
+        case 'Add a department':
+            // code block
+            // let departmentAnswer = await inquirer.prompt(departmentQuestions);
+            await addDepartment();
+            nextActionPrompt();
+            break;
+        case 'Add a role':
+            // code block
+            // let roleAnswer = await inquirer.prompt(roleQuestions);
+            await addRole();
+            nextActionPrompt();
+            break;
+        case 'Add an employee':
+            // code block
+            // let employeeAnswer = inquirer.prompt(employeeQuestions);
+            await addEmployee();
+            nextActionPrompt();
+            break;
+        case 'Update an employee role':
+            // code block
+            // let updateAnswer = await inquirer.prompt(updateQuestions)
+            await updateRole();
+            nextActionPrompt();
+            break;
+        case 'Exit the application':
+            // code block
+            process.exit(0);
+    }
 };
 
 nextActionPrompt();
